699874f1c376e6c1a3d01e269d451331c2071596
Shrinath Suryawanshi <46258187+shrinath56@users.noreply.github.com>
2022-11-20 13:44:50 +0530

    Add files via upload

---
+++ ImageSearchEngine/src/api/unsplash.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.unsplash.com',
+  headers: {
+    Authorization:
+      'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
+  }
+});
+
+
+
+//Axios is popular library is mainly used to send asynchronous HTTP requests to REST endpoints.
